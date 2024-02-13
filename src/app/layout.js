82e21eb4e2695ec35292564510789ac66d32a392
Sean-Patrick <rowan.seanp@gmail.com>
2024-02-13 12:57:41 -0500

    Initial Next.js set up and created layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,13 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +17,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="h-screen w-screen flex justify-center items-center">
+        <Card className="h-[500px] w-[800px] bg-red-500 border border-black shadow-2xl">
+          <CardContent>
+            {children}
+          </CardContent>
+        </Card>
+      </body>
     </html>
   );
 }
