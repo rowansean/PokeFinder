40ff6973a19c08cb9df845ecccaf55d4f9ce21f4
cwburklin12 <clayton.burklin@gmail.com>
2024-02-13 15:13:18 -0500

    feat: created welcome page component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
+import Welcome from "@/components/ui/welcome";
 
 export default function Home() {
   return (
     <main className="">
-      <h1 className="text-4xl font-bold text-black">Welcome to PokeFinder!</h1>
-      <h2 className="text-2xl font-semibold text-black">Find your favorite Pokemon</h2>
+     <Welcome />
     </main>
   );
 }
---
+++ src/components/ui/welcome.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <div className='mt-[100px] pt-20 text-center text-white'>
+      <h1 className='text text-4xl font-bold '>Welcome to</h1>
+      <div className="flex w-full overflow-hidden">
+        <img src="/PokeBall.png" alt="placeholder" className='border border-black w-16 h-16' />
+        <img src="PokeFinder-Logo.png" alt="placeholder" className='w-38 h-16'/>
+      </div>
+      <p className='text-lg font-bold'>Search to get started!</p>
+    </div>
+  );
+}
+    
+  
+
